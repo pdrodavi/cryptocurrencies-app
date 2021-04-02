613acb3ed168fa34c45a904f43ad17f5a3adc34c
pedrodavi <pdrodavi@outlook.com>
2021-04-01 22:42:36 -0300

    update controller

--- src/main/java/br/com/pedrodavi/cryptocurrencies/controller/HomeController.java
+++ src/main/java/br/com/pedrodavi/cryptocurrencies/controller/HomeController.java
@@ -1,20 +1,20 @@
 package br.com.pedrodavi.cryptocurrencies.controller;
 
+import br.com.pedrodavi.cryptocurrencies.service.impl.QueryCoinsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import br.com.pedrodavi.cryptocurrencies.service.impl.QueryCoinsServiceImpl;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 
-@RestController
+@Controller
 public class HomeController {
 
     @Autowired private QueryCoinsServiceImpl service;
