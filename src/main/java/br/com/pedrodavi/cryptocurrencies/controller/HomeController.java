2d8716cb125686d0e430f2c29a1da7a164dc8062
Pedro Davi <31486060+pdrodavi@users.noreply.github.com>
2021-04-01 22:29:35 -0300

    Update HomeController.java

--- src/main/java/br/com/pedrodavi/cryptocurrencies/controller/HomeController.java
+++ src/main/java/br/com/pedrodavi/cryptocurrencies/controller/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,7 +14,7 @@ import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 
-@Controller
+@RestController
 public class HomeController {
 
     @Autowired private QueryCoinsServiceImpl service;
